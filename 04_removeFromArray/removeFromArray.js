4ab5fd7b5bedde188a0fa3a846899643a8d8770d
rsgerrish <rbn.sills@gmail.com>
2022-04-09 10:51:17 -0400

    Complete Lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+function removeFromArray(arrayToCheck, ...argsToRemove) {  
+  let returnArray = arrayToCheck.filter(arr => !argsToRemove.includes(arr));
 
-};
+  console.log(returnArray);
+  return returnArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
