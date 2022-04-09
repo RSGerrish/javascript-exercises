7f6085e92ece5ed2be201d6c74cd7ef9dffd0800
rsgerrish <rbn.sills@gmail.com>
2022-04-08 23:00:09 -0400

    Complete Lesson 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+  let returnStr = "";
 
+  if (n < 0) {
+    return "ERROR";
+  } else {
+    for (i = 0; i < n; i++) {
+      returnStr += str;
+    }
+  }
+
+  return returnStr;
 };
 
 // Do not edit below this line
